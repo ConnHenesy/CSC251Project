85757b8c1aac37d1011b80490f4b8198ca40466e
ConnHenesy <chenesy0@email.cpcc.edu>
2024-11-11 08:20:49 -0500

    Added code to display all policy information, the number of smokers, and the number of non-smokers

--- Project_conn_henesy.java
+++ Project_conn_henesy.java
@@ -8,40 +8,61 @@ public class Project_conn_henesy
       File file = new File("PolicyInformation.txt");
       Scanner inputFile = new Scanner(file);
       
-      int policyNo = inputFile.nextInt();
-      inputFile.nextLine();
-            
-      String provider = inputFile.nextLine();
+      int totalSmokers = 0;
+      int totalNonSmokers = 0;
       
-      String firstName = inputFile.nextLine();
-      
-      String lastName = inputFile.nextLine();
-      
-      int age = inputFile.nextInt();
-      inputFile.nextLine();
-      
-      String smokingStatus = inputFile.nextLine();
-      
-      double height = inputFile.nextDouble();
-      inputFile.nextLine();
-      
-      double weight = inputFile.nextDouble();
-      inputFile.nextLine();
-
-
-
-      Policy user = new Policy(policyNo, provider, firstName, lastName, age, smokingStatus, height, weight);
-      
-      System.out.print("\n\n\n\n");
-      System.out.printf("Policy Number: %d\n",user.getPolicyNumber());
-      System.out.printf("Provider Name: %s\n",user.getProviderName());
-      System.out.printf("Policyholder's First Name: %s\n",user.getFirstName());
-      System.out.printf("Policyholder's Last Name: %s\n",user.getLastName());
-      System.out.printf("Policyholder's Age: %d\n",user.getAge());
-      System.out.printf("Policyholder's Smoking Status: %s\n",user.getSmokingStatus());
-      System.out.printf("Policyholder's Height: %.1f\n",user.getHeight());
-      System.out.printf("Policyholder's Weight: %.1f\n",user.getWeight());
-      System.out.printf("Policyholder's BMI: %.2f\n",user.findBMI(height, weight));
-      System.out.printf("Policy Price: $%.2f\n",user.findPolicyPrice(age, smokingStatus, height, weight));
+      while (inputFile.hasNext())
+      {
+         int policyNo = inputFile.nextInt();
+         inputFile.nextLine();
+               
+         String provider = inputFile.nextLine();
+         
+         String firstName = inputFile.nextLine();
+         
+         String lastName = inputFile.nextLine();
+         
+         int age = inputFile.nextInt();
+         inputFile.nextLine();
+         
+         String smokingStatus = inputFile.nextLine();
+         
+         double height = inputFile.nextDouble();
+         inputFile.nextLine();
+         
+         double weight = inputFile.nextDouble();
+         inputFile.nextLine();
+         if (inputFile.hasNext())
+         {
+            inputFile.nextLine();
+         }
+   
+   
+   
+         Policy user = new Policy(policyNo, provider, firstName, lastName, age, smokingStatus, height, weight);
+         
+         System.out.print("\n\n");
+         System.out.printf("Policy Number: %d\n",user.getPolicyNumber());
+         System.out.printf("Provider Name: %s\n",user.getProviderName());
+         System.out.printf("Policyholder's First Name: %s\n",user.getFirstName());
+         System.out.printf("Policyholder's Last Name: %s\n",user.getLastName());
+         System.out.printf("Policyholder's Age: %d\n",user.getAge());
+         System.out.printf("Policyholder's Smoking Status: %s\n",user.getSmokingStatus());
+         System.out.printf("Policyholder's Height: %.1f\n",user.getHeight());
+         System.out.printf("Policyholder's Weight: %.1f\n",user.getWeight());
+         System.out.printf("Policyholder's BMI: %.2f\n",user.findBMI(height, weight));
+         System.out.printf("Policy Price: $%.2f\n",user.findPolicyPrice(age, smokingStatus, height, weight));
+         
+         if (user.getSmokingStatus().equals("smoker"))
+         {
+            totalSmokers += 1;
+         }
+         else
+         {
+            totalNonSmokers += 1;
+         }
+      }
+      System.out.println("\n\nThe number of policies with a smoker is: " + totalSmokers);
+      System.out.println("The number of policies with a non-smoker is: " + totalNonSmokers);
    }
 }

13d5103e7426704700d505cd19eb1ff7b3154b3a
ConnHenesy <chenesy0@email.cpcc.edu>
2024-11-11 06:58:05 -0500

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -12,6 +12,9 @@ public class Policy
    
    //Constructors 
    
+   /**
+   No-arg constructor
+   */
    
    public Policy()
    {
@@ -24,6 +27,19 @@ public class Policy
       height = 0;
       weight = 0;
    }
+   
+   
+   /**
+   Constructor that takes arguments
+   @param num1 The user's policy number
+   @param num2 The user's provider's name
+   @param num3 The user's first name
+   @param num4 The user's surname
+   @param num5 The user's age
+   @param num6 Whether the user is a smoker ("smoker" or "non-smoker")
+   @param num7 The user's height, in inches
+   @param num8 The user's weight, in pounds
+   */
    
    public Policy(int no, String prov, String first, String last, int a, String smoke, double h, double w)
    {
@@ -41,41 +57,81 @@ public class Policy
    //Setters
    
    
+   /**
+   Mutater for the user's policy number
+   @param1 The user's policy number
+   */
+   
    public void setPolicyNumber(int no)
    {
       policyNo = no;
    }
+   
+   /**
+   Mutater for the user's provider's name
+   @param1 The user's provider's name
+   */
    
    public void setProviderName(String prov)
    {
       provider = prov;
    }
    
+   /**
+   Mutater for the user's first name
+   @param1 The user's first name
+   */
+   
    public void setFirstName(String first)
    {
       firstName = first;
    }
    
+   /**
+   Mutater for the user's last name
+   @param1 The user's surname
+   */
+
    public void setLastName(String last)
    {
       lastName = last;
    }
    
+   /**
+   Mutater for the user's age
+   @param1 The user's age
+   */
+
    public void setAge(int a)
    {
       age = a;
    }
    
+   /**
+   Mutater for the user's smoking status
+   @param1 Whether the user is a smoker ("smoker" or "non-smoker")
+   */
+
    public void setSmokingStatus(String smoke)
    {
       smokingStatus = smoke;
    }
    
+   /**
+   Mutater for the user's height
+   @param1 The user's height, in inches
+   */
+
    public void setHeight(double h)
    {
       height = h;
    }
    
+   /**
+   Mutater for the user's weight
+   @param1 The user's weight, in pounds
+   */
+
    public void setWeight(double w)
    {
       weight = w;
@@ -85,41 +141,81 @@ public class Policy
    //Getters
 
 
+   /**
+   Accessor for the user's policy number
+   @return The user's policy number
+   */
+   
    public int getPolicyNumber()
    {
       return policyNo;
    }
+   
+   /**
+   Accessor for the user's provider's name
+   @return The user's provider's name
+   */
    
    public String getProviderName()
    {
       return provider;
    }
    
+   /**
+   Accessor for the user's first name
+   @return The user's first name
+   */
+   
    public String getFirstName()
    {
       return firstName;
    }
+   
+   /**
+   Accessor for the user's last name
+   @return The user's surname
+   */
    
    public String getLastName()
    {
       return lastName;
    }
    
+   /**
+   Accessor for the user's age
+   @return The user's age
+   */
+
    public int getAge()
    {
       return age;
    }
    
+   /**
+   Accessor for the user's smoking status
+   @return Whether the user is a smoker ("smoker" or "non-smoker")
+   */
+
    public String getSmokingStatus()
    {
       return smokingStatus;
    }
    
+   /**
+   Accessor for the user's height
+   @return The user's height, in inches
+   */
+
    public double getHeight()
    {
       return height;
    }
    
+   /**
+   Accessor for the user's weight
+   @return The user's weight, in pounds
+   */
+
    public double getWeight()
    {
       return weight;
@@ -128,11 +224,26 @@ public class Policy
    
    //Calculators
    
+   /**
+   Calculator for the user's body mass index (BMI)
+   @param1 The user's height
+   @param2 The user's weight
+   @return The user's BMI
+   */
    
    public double findBMI(double height, double weight)
    {
       return ((weight*703)/(Math.pow(height,2)));
    }
+   
+   /**
+   Calculator for the price of the user's policy plan
+   @param1 The user's age
+   @param2 Whether the user is a smoker ("smoker" or "non-smoker")
+   @param3 The user's height
+   @param4 The user's weight
+   @return The total price of the user's policy plan
+   */
    
    public double findPolicyPrice(int age, String smokingStatus, double height, double weight)
    {
